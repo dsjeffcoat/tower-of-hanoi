f85d08cfc48b2ff7cc49753e834444dbc57b1f21
Diarte Jeffcoat <diartesjeffcoat@icloud.com>
2019-12-18 12:59:36 -0500

    Updated with fixed code.

--- hanoi.js
+++ hanoi.js
@@ -1,31 +1,33 @@
 // Tower of Hanoi project by Garnetta Matthews-Lincoln & Diarte Jeffcoat
 
-let choose=true;
+
+let choose = true;
 let chosen;
-function move (click)
-{
-     let currentClick=click.currentTarget
-     if(choose){
-          chosen=currentClick.lastElementChild;
-          document.getElementById('holding').appendChild(chosen)
-          choose=false
-     }
-     else if (!choose)
-          if (!currentClick.lastElementChild)
-          {
-               currentClick.appendChild(chosen)
-               choose=true
-          }
-          else if (Number(chosen.id)<Number  (currentClick.lastElementChild.id))
-          {
-          currentClick.appendChild(chosen)
-          choose=true
-          }
-     if (document.getElementById('tower3').childElementCount===4)
-     {
-         document.getElementById("ewa").style.visibility="visible" 
-     }
-}
+
+const move = function (click) {
+      let currentClick = click.currentTarget;
+//       console.log(currentClick);
+      if (choose){
+           chosen = currentClick.lastElementChild;
+           document.getElementById('holding').appendChild(chosen);
+       //     console.log(chosen);
+           choose = false;
+      } 
+      else if (!choose) {
+           if (!currentClick.lastElementChild) {
+                  currentClick.appendChild(chosen);
+                  choose = true;
+           } else if (Number(chosen.dataset.weight) < Number(currentClick.lastElementChild.dataset.weight)) {
+                  currentClick.appendChild(chosen);
+                  choose = true;
+           }
+      }
+
+      if (document.getElementById('tower3').childElementCount === 4) {
+          document.getElementById("ewa").style.visibility="visible"; 
+      }
+ }
+
 document.getElementById('tower1').addEventListener('click', move);
 document.getElementById('tower2').addEventListener('click', move);
 document.getElementById('tower3').addEventListener('click', move);
